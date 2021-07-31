906d4eb093959ee1fb36c1bd02d94f35c16ef685
LeonardoFerrisi <86019814+LeonardoFerrisi@users.noreply.github.com>
2021-07-31 15:26:55 -0400

    added trainer.py

---
+++ prototyping/trainer.py
@@ -0,0 +1,5 @@
+# Train model to detect teeth clenching -> Instrument use (Will add user ability to pause between sounds)
+# Model should have both cases of non teeth clenching and teeth clenching
+# Mins and maxes of respective channel average pairs (can be obtained from training data set)
+# Mins and maxes will be used to map eeg data to given (screen) theremin amp and freq params
+#
