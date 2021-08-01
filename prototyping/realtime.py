f21a634e92238cf163a6ecfcdf9492701af12c71
LeonardoFerrisi <86019814+LeonardoFerrisi@users.noreply.github.com>
2021-07-31 20:54:43 -0400

    Changes by nemie

--- prototyping/realtime.py
+++ prototyping/realtime.py
@@ -45,24 +45,50 @@ class DataThread(threading.Thread):
             #reshape_data = data.T
             #print(reshape_data.shape, data.shape)
 
-            df = pd.DataFrame(reshape_data)
-            fft_data = np.fft.fft2(reshape_data)
-            fft_df = pd.DataFrame(fft_data)
+            #df = pd.DataFrame(reshape_data)
+            #fft_data = np.fft.fft2(reshape_data)
+            #fft_df = pd.DataFrame(fft_data)
             #df = df.transpose()
             #print(df.values[0],df.values[2])
 
             #print(len(reshape_data))
             # print('DataShape %s :' % (str(data.shape)) + str(type(data)))
 
-            for channel in self.eeg_channels:
+            nfft = DataFilter.get_nearest_power_of_two(self.samplingRate)
+            myLen = len(data)
+            sizeB = data.size
+            shapeOfYou = data.shape
+            print(nfft)
+            print(len(data))
+            print(data.size)
+            # time.sleep(20)
+
+            for channel in self.eeg_channels[0:7]:
+
                 # make filters work in place
                 # fftData = DataFilter.perform_fft(data[channel], 2)
-                DataFilter.perform_bandstop(data[channel], self.samplingRate, 50.0, 4.0, 4,
-                                            FilterTypes.BUTTERWORTH.value, 0)  # bandstop 48-52
-                DataFilter.perform_bandstop(data[channel], self.samplingRate, 60.0, 4.0, 4,
-                                            FilterTypes.BUTTERWORTH.value, 0)  # bandstop 58 - 62
-                DataFilter.perform_bandpass(data[channel], self.samplingRate, 21.0, 20.0, 4,
-                                            FilterTypes.BESSEL.value, 0)  # bandpass 11 - 31
+                # DataFilter.perform_bandstop(data[channel], self.samplingRate, 50.0, 4.0, 4,
+                #                             FilterTypes.BUTTERWORTH.value, 0)  # bandstop 48-52
+                # DataFilter.perform_bandstop(data[channel], self.samplingRate, 60.0, 4.0, 4,
+                #                             FilterTypes.BUTTERWORTH.value, 0)  # bandstop 58 - 62
+                # DataFilter.perform_bandpass(data[channel], self.samplingRate, 21.0, 20.0, 4,
+                #                             FilterTypes.BESSEL.value, 0)  # bandpass 11 - 31
+
+                # DataFilter.detrend(data[channel], DetrendOperations.LINEAR.value)
+                # DataFilter.detrend(data[channel], DetrendOperations.LINEAR.value)
+                DataFilter.perform_rolling_filter(data[channel], 3, AggOperations.MEDIAN.value)
+                DataFilter.perform_lowpass(data[channel], self.samplingRate, 50.0, 5,
+                                           FilterTypes.CHEBYSHEV_TYPE_1.value, 1)
+
+                psd = DataFilter.get_psd_welch(data[channel], nfft, nfft // 2, self.samplingRate,
+                                               WindowFunctions.BLACKMAN_HARRIS.value)
+
+                band_power_alpha = DataFilter.get_band_power(psd, 7.0, 13.0)
+                band_power_beta = DataFilter.get_band_power(psd, 14.0, 30.0)
+
+                outputFile = open("output.txt", "w")
+                outputFile.write('[' + str(time.time()) + '] Alpha: ' +  str(band_power_alpha) + 'Beta: ' + str(band_power_beta) + '\n  ')
+                print("alpha/beta:%f", band_power_alpha / band_power_beta )
             # print(fftData)
 
 
